50399f3f242af0f109ebb9f206f53902a633b630
David Weschler <davidweschler8@gmail.com>
2024-10-10 00:25:31 +0300

    added the project section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.getElementById('navbar');
+const skillItems = document.querySelectorAll('.skill-item');
 
 // Hamburger Menu:
 hamburger.addEventListener('click', function () {
@@ -12,3 +13,17 @@ hamburger.addEventListener('click', function () {
 document.getElementById('cv-btn').onclick = function () {
   window.open('./assets/cv.pdf', '_blank');
 };
+
+skillItems.forEach(item => {
+  const img = item.querySelector('img');
+  const originalSrc = img.src;
+  const hoverSrc = originalSrc.replace('.png', '-hover.png');
+
+  item.addEventListener('mouseenter', () => {
+    img.src = hoverSrc;
+  });
+
+  item.addEventListener('mouseleave', () => {
+    img.src = originalSrc;
+  });
+});
