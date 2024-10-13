d0de003d0488d071c15b0a408dc32829ed604388
David Weschler <davidweschler8@gmail.com>
2024-10-13 18:34:22 +0300

    added some security changes

---
+++ public/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.getElementById('navbar');
+const menuItems = document.querySelectorAll('#navbar a');
+const skillItems = document.querySelectorAll('.skill-item');
+
+// Hamburger Menu:
+hamburger.addEventListener('click', function () {
+  navbar.classList.toggle('active'); // Show/hide the menu
+});
+
+// Close the menu when a menu item is clicked
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navbar.classList.remove('active'); // Hide the menu
+  });
+});
+
+document.getElementById('contact-btn').addEventListener('click', function () {
+  document.getElementById('contact').scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+//Download CV Button:
+document.getElementById('cv-btn').onclick = function () {
+  window.open('./assets/cv.pdf', '_blank');
+};
+
+skillItems.forEach(item => {
+  const img = item.querySelector('img');
+  const originalSrc = img.src;
+  const hoverSrc = originalSrc.replace('.png', '-hover.png');
+
+  item.addEventListener('mouseenter', () => {
+    img.src = hoverSrc;
+  });
+
+  item.addEventListener('mouseleave', () => {
+    img.src = originalSrc;
+  });
+});
+
+//--------------------
+const contactForm = document.querySelector('.contact-me-form');
+
+let fullName = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let formData = {
+    name: fullName.value,
+    email: email.value,
+    message: message.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+
+    if (xhr.responseText == 'success') {
+      alert('Message Sent');
+      fullName.value = '';
+      email.value = '';
+      message.value = '';
+    } else {
+      alert('Something went wrong, try again');
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+require("dotenv").config(); // Load environment variables
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER, // Email from .env
+      pass: process.env.EMAIL_PASS, // Password from .env
+    },
+  });
+
+  const mailOptions = {
+    from: `${req.body.name} <${req.body.email}>`,
+    to: process.env.EMAIL_USER,
+    subject: "New Contact Form Submission", // Subject of the email
+    text: `You have a new message from ${req.body.name} (${req.body.email}):\n\n${req.body.message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent");
+      res.send("success");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
